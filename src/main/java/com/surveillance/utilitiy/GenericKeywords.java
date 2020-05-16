71ca121ee92fee84bc453caf86668c8061043364
Adam Lowe <alowe@youniqueproducts.com>
2020-05-15 20:48:19 -0600

    updating path

--- src/main/java/com/surveillance/utilitiy/GenericKeywords.java
+++ src/main/java/com/surveillance/utilitiy/GenericKeywords.java
@@ -48,7 +48,7 @@ public class GenericKeywords extends ApplicationKeywords {
 			logger.info("openBrowser action is started");
 			System.setProperty("webdriver.chrome.driver",
 					//System.getProperty("user.dir") + "/webDrivers/chromedriver.exe");
-					 "/var/lib/jenkins/tools/chromedriver");
+					 "/var/lib/jenkins/tools/chromedriver.exe");
 			driver = new ChromeDriver();
 		} else if (browserName.equalsIgnoreCase("IE")) {
 			DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
