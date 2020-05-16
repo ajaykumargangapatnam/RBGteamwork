13e6fc6ebced6a04dfb9d234a9c5504a1b52edfd
Adam Lowe <alowe@youniqueproducts.com>
2020-05-15 23:11:25 -0600

    trying something

--- src/main/java/com/surveillance/utilitiy/GenericKeywords.java
+++ src/main/java/com/surveillance/utilitiy/GenericKeywords.java
@@ -47,8 +47,8 @@ public class GenericKeywords extends ApplicationKeywords {
 		if (browserName.equalsIgnoreCase("Chrome")) {
 			logger.info("openBrowser action is started");
 			System.setProperty("webdriver.chrome.driver",
-					//System.getProperty("user.dir") + "/webDrivers/chromedriver.exe");
-					 "/var/lib/jenkins/tools/chromedriver/chromedriver");
+					System.getProperty("user.dir") + "/webDrivers/chromedriver.exe");
+					 //"/var/lib/jenkins/tools/chromedriver/chromedriver");
 			//adam added this
 			ChromeOptions chromeOptions = new ChromeOptions();
 			//chromeOptions.addArguments("--headless");
