711ab363599385bf66fce665866b827bf8040d5b
Rajasekhar <rajasekharsadu9421@gmail.com>
2024-05-02 09:35:57 +0530

    2-05-2024 commit

--- src/main/java/practicepage/OrangeHRM_LoginPage.java
+++ src/main/java/practicepage/OrangeHRM_LoginPage.java
@@ -1,13 +1,22 @@
 package practicepage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.idealized.Javascript;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.surveillance.utilitiy.ApplicationKeywords;
 import com.surveillance.utilitiy.GenericKeywordsWithPage;
+
+import ch.qos.logback.core.util.Duration;
 
 public class OrangeHRM_LoginPage extends ApplicationKeywords
 {
@@ -67,7 +76,40 @@ public class OrangeHRM_LoginPage extends ApplicationKeywords
 	@FindBy(xpath="(//span[@class='oxd-switch-input oxd-switch-input--active --label-right'])[1]")
 	WebElement  subscripadd;
 	
-	GenericKeywordsWithPage keywords = new GenericKeywordsWithPage("OrangeHRM_Login");
+	@FindBy(xpath="//a[text()='Localization']")
+	WebElement  localization;
+	
+	@FindBy(xpath="//div[text()='English (United States)']/following::div[1]/i")
+	WebElement langbox;
+	
+	@FindBy(xpath="//span[text()='Dutch - Nederlands']")
+	WebElement language;
+	
+	@FindBy(xpath="//div[text()='yyyy-dd-mm ( 2024-01-05 )']/following::div[1]/i")
+	WebElement  datebox; 
+	
+	@FindBy(xpath="//span[text()='mm-dd-yyyy ( 05-01-2024 )']")
+	WebElement  dateyear;
+	
+	@FindBy(xpath="//button[text()=' Save ']")
+	WebElement  localsave;
+	
+	@FindBy(xpath="//a[text()='Language Packages']")
+	WebElement langpackages;
+	
+	@FindBy(xpath="//button[text()=' Add ']")
+	WebElement lpaddbtn;
+	
+	@FindBy(xpath="//label[text()='Name']/following::div/div/div/div[@class='oxd-select-text-input']")
+	WebElement addlanguage;
+	
+	@FindBy(xpath="//span[text()='Afrikaans (Namibia) - Afrikaans (Namibië)']")
+	WebElement arabicdata;
+	
+	@FindBy(xpath="//button[text()=' Save ']")
+	WebElement langsave;
+	
+	GenericKeywordsWithPage keywords = new GenericKeywordsWithPage("OrangeHRM_LoginPage");
 
 	public void enter_Username(String user)
 	{
@@ -121,6 +163,11 @@ public class OrangeHRM_LoginPage extends ApplicationKeywords
 		keywords.click(savebtn);
 	}
 	
+	public void Clickon_Save()
+	{
+		keywords.click(savebtn);
+	}
+	
 	public void Clickon_Reset()
 	{
 		keywords.click(resetbtn);
@@ -156,5 +203,277 @@ public class OrangeHRM_LoginPage extends ApplicationKeywords
 		keywords.click(emailsavebtn);
 	}
 	
+	public void Clickon_Localization()
+	{
+		keywords.click(localization);
+	}
 	
+	public void Enter_CountryLanguage() throws Throwable
+	{
+		keywords.click(langbox);
+		Thread.sleep(10000);
+		keywords.click(language);
+	}
+	
+	public void Enter_DOY() throws Throwable
+	{
+		keywords.click(datebox);
+		Thread.sleep(10000);
+		keywords.click(dateyear);
+	}
+	
+	public void Clickon_LocalSave()
+	{
+		keywords.click(localsave);
+	}
+	
+	public void Clickon_LanguagePackages()
+	{
+		keywords.click(langpackages);
+	}
+	
+	public void Clickon_LPAddBtn()
+	{
+		keywords.click(addbtn);
+	}
+	
+	public void Enter_language() throws Throwable
+	{
+		keywords.click(addlanguage);
+		Thread.sleep(3000);
+		//keywords.movetoElement(arabicdata);
+		keywords.click(arabicdata);
+	}
+	
+	public void Clickon_LangSaveBox()
+	{
+		keywords.click(langsave);
+	}
+	
+	@FindBy(xpath="//a[text()='Modules']")
+	WebElement modules;
+	public void Clickon_Modules()
+	{
+		keywords.click(modules);
+	}
+	
+	@FindBy(xpath="//p[text()='Leave Module']/parent::div/div/label/span")
+	WebElement leavemodule;
+	public void Clickon_LeaveModule()
+	{
+		keywords.click(leavemodule);
+	}
+	
+	@FindBy(xpath="//p[text()='Time Module']/parent::div/div/label/span")
+	WebElement time;
+	public void Clickon_TimeModule()
+	{
+		keywords.click(time);
+	}
+	
+	@FindBy(xpath="//p[text()='Recruitment Module']/parent::div/div/label/span")
+	WebElement recruitmodule;
+	public void Clickon_RecruitmentModule()
+	{
+		keywords.click(recruitmodule);
+	}
+	
+	@FindBy(xpath="//p[text()='Performance Module']/parent::div/div/label/span")
+	WebElement performsncemodule;
+	public void Clickon_PerformanceModule()
+	{
+		keywords.click(performsncemodule);
+	}
+	
+	@FindBy(xpath="//p[text()='Directory Module']/parent::div/div/label/span")
+	WebElement directorymodule;
+	public void Clickon_DirectoryModule()
+	{
+		keywords.click(directorymodule);
+	}
+	
+	@FindBy(xpath="//p[text()='Maintenance Module']/parent::div/div/label/span")
+	WebElement maintanancemodule;
+	public void Clickon_maintananceModule()
+	{
+		keywords.click(maintanancemodule);
+	}
+	
+	@FindBy(xpath="//p[text()='Mobile']/parent::div/div/label/span")
+	WebElement mobile;
+	public void Clickon_MobileModule()
+	{
+		keywords.click(mobile);
+	}
+	
+	@FindBy(xpath="//p[text()='Claim Module']/parent::div/div/label/span")
+	WebElement claimmodule;
+	public void Clickon_ClaimModule()
+	{
+		keywords.click(claimmodule);
+	}
+	
+	@FindBy(xpath="//button[text()=' Save ']")
+	WebElement modulesave;
+	public void Clickon_ModuleSave()
+	{
+		keywords.click(modulesave);
+	}
+	
+	@FindBy(xpath="//a[text()='Social Media Authentication']")
+	WebElement socialauthentication;
+	public void SocialMediaAuthentication()
+	{
+		keywords.click(socialauthentication);
+	}
+	
+	@FindBy(xpath="//button[text()=' Add ']")
+	WebElement socialaddbtn;
+	public void SocialAddBtn()
+	{
+		keywords.click(socialaddbtn);
+	}
+	
+	@FindBy(xpath="//label[text()='Name']/parent::div/parent::div[@class='oxd-input-group oxd-input-field-bottom-space']/child::div/following-sibling::div/input")
+	WebElement nameinputbox;
+	public void NameInputBox(String ndata)
+	{
+		keywords.enterData(nameinputbox,ndata);
+	}
+	
+	@FindBy(xpath="//label[text()='Provider URL']/parent::div/parent::div[@class='oxd-input-group oxd-input-field-bottom-space']/child::div/following-sibling::div/input")
+	WebElement providerinputbox;
+	public void ProviderInputBox(String pdata)
+	{
+		keywords.enterData(providerinputbox,pdata);
+	}
+	
+	@FindBy(xpath="//label[text()='Client ID']/parent::div/parent::div[@class='oxd-input-group oxd-input-field-bottom-space']/child::div/following-sibling::div/input")
+	WebElement clientinputbox;
+	public void ClientInputBox(String cdata)
+	{
+		keywords.enterData(clientinputbox,cdata);
+	}
+	
+	@FindBy(xpath="//label[text()='Client Secret']/parent::div/parent::div[@class='oxd-input-group oxd-input-field-bottom-space']/child::div/following-sibling::div/input")
+	WebElement clientsecterinputbox;
+	public void ClientsecretInputBox(String csdata)
+	{
+		keywords.enterData(clientsecterinputbox, csdata);
+	}
+	
+	@FindBy(xpath="//button[text()=' Save ']")
+	WebElement socialsave;
+	public void Clickon_SocialSave()
+	{
+		keywords.click(socialsave);
+	}
+		
+	@FindBy(xpath="//div[text()='Actions']/following::div/div/div/div/following-sibling::div/following-sibling::div/child::div[@class='oxd-table-cell-actions']/button/following::button")
+	WebElement editbtn;
+	public void Clickon_EditBtn()
+	{
+		keywords.click(editbtn);
+	}
+	
+	public void EditNameInputBox(String ndata) throws Throwable
+	{
+		keywords.click(nameinputbox);
+		Thread.sleep(3000);
+		Actions a1 = new Actions(keywords.driver);
+		a1.keyDown(Keys.CONTROL);
+		a1.sendKeys("a");
+		a1.keyUp(Keys.CONTROL).perform();
+		Thread.sleep(3000);
+		a1.keyDown(Keys.BACK_SPACE).perform();
+		
+		Thread.sleep(3000);
+		a1.keyUp(Keys.CONTROL).sendKeys(ndata);
+		a1.keyUp(Keys.CONTROL).perform();
+		
+	}
+	
+	public void EditProviderInputBox(String pdata) throws Throwable
+	{
+		keywords.click(providerinputbox);
+		Thread.sleep(3000);
+		Actions a1 = new Actions(keywords.driver);
+		a1.keyDown(Keys.CONTROL);
+		a1.sendKeys("a");
+		a1.keyUp(Keys.CONTROL).perform();
+		Thread.sleep(3000);
+		a1.keyDown(Keys.BACK_SPACE).perform();
+		
+		Thread.sleep(3000);
+		a1.keyUp(Keys.CONTROL).sendKeys(pdata);
+		a1.keyUp(Keys.CONTROL).perform();
+	}
+	
+	
+	public void EditClientInputBox(String cdata) throws Throwable
+	{
+		keywords.click(clientinputbox);
+		Thread.sleep(3000);
+		Actions a1 = new Actions(keywords.driver);
+		a1.keyDown(Keys.CONTROL);
+		a1.sendKeys("a");
+		a1.keyUp(Keys.CONTROL).perform();
+		Thread.sleep(3000);
+		a1.keyDown(Keys.BACK_SPACE).perform();
+		
+		Thread.sleep(3000);
+		a1.keyUp(Keys.CONTROL).sendKeys(cdata);
+		a1.keyUp(Keys.CONTROL).perform();
+	}
+	
+	public void EditClientsecretInputBox(String csdata) throws Throwable
+	{
+		keywords.click(clientsecterinputbox);
+		Thread.sleep(3000);
+		Actions a1 = new Actions(keywords.driver);
+		a1.keyDown(Keys.CONTROL);
+		a1.sendKeys("a");
+		a1.keyUp(Keys.CONTROL).perform();
+		Thread.sleep(3000);
+		a1.keyDown(Keys.BACK_SPACE).perform();
+		
+		Thread.sleep(3000);
+		a1.keyUp(Keys.CONTROL).sendKeys(csdata);
+		a1.keyUp(Keys.CONTROL).perform();
+	}
+	
+	@FindBy(xpath="//a[text()='Register OAuth Client']")
+	WebElement registerclient;
+	public void RegisterOAuthClient()
+	{
+		keywords.click(registerclient);
+	}
+	
+	@FindBy(xpath="//button[text()=' Add ']")
+	WebElement OAuthAddbtn;
+	public void OAuthAddbtn()
+	{
+		keywords.click(OAuthAddbtn);
+	}
+	
+	@FindBy(xpath="//label[text()='Name']/parent::div/following::div[1]/input")
+	WebElement oauthinputbox;
+	public void Enter_OAuthInputName(String oadata)
+	{
+		keywords.enterData(oauthinputbox, oadata);
+	}
+	
+	@FindBy(xpath="//label[text()='Redirect URI']/parent::div/following::div[1]/input")
+	WebElement requiredinputbox;
+	public void Enter_RedirectURIName(String rdata)
+	{
+		keywords.enterData(requiredinputbox, rdata);
+	}
+	
+	@FindBy(xpath="//button[text()=' Save ']")
+	WebElement oauthsavebtn;
+	public void OAuthSavebtn()
+	{
+		keywords.click(oauthsavebtn);
+	}
 }
--- src/test/java/PracticeTest/OrangeHRM_ConfigTest.java
+++ src/test/java/PracticeTest/OrangeHRM_ConfigTest.java
@@ -134,4 +134,154 @@ public class OrangeHRM_ConfigTest extends BaseTest
 		Thread.sleep(5000);
 		
 	}
+	
+	@Test(priority=5 , dataProvider="setData0")
+	public void ClickonLocalization(Hashtable<String, String>h1) throws Throwable
+	{
+		extentLoggerECP = parentExtentLogger.createNode("user can click on Localization");
+		
+		oc = new OrangeHRM_LoginPage(GenericKeywordsWithPage.driver);
+		
+		oc.enter_Username(h1.get("userName"));
+		oc.enter_Password(h1.get("password"));
+		oc.click_Login();
+		oc.Clickon_Admin();
+		oc.Clickon_Configuration();
+		Thread.sleep(3000);
+		oc.Clickon_Localization();
+		Thread.sleep(3000);
+		oc.Enter_CountryLanguage();
+		Thread.sleep(3000);
+		oc.Enter_DOY();
+		Thread.sleep(3000);
+		oc.Clickon_LocalSave();
+		Thread.sleep(5000);
+	}
+	
+	@Test(priority=6 , dataProvider="setData0")
+	public void ClickonLanguagePackages(Hashtable<String, String>h1) throws Throwable
+	{
+		extentLoggerECP = parentExtentLogger.createNode("user can click on LanguagePackages ");
+		
+		oc = new OrangeHRM_LoginPage(GenericKeywordsWithPage.driver);
+		
+		oc.enter_Username(h1.get("userName"));
+		oc.enter_Password(h1.get("password"));
+		oc.click_Login();
+		oc.Clickon_Admin();
+		oc.Clickon_Configuration();
+		Thread.sleep(3000);
+		oc.Clickon_LanguagePackages();
+		Thread.sleep(3000);
+		oc.Clickon_LPAddBtn();
+		Thread.sleep(3000);
+		oc.Enter_language();
+		Thread.sleep(3000);
+		oc.Clickon_LangSaveBox();
+		Thread.sleep(10000);
+	}
+	
+	@Test(priority=7 , dataProvider="setData0")
+	public void ClickonModules(Hashtable<String, String>h1) throws Throwable
+	{
+		extentLoggerECP = parentExtentLogger.createNode("user can click on Modules ");
+		
+		oc = new OrangeHRM_LoginPage(GenericKeywordsWithPage.driver);
+		
+		oc.enter_Username(h1.get("userName"));
+		oc.enter_Password(h1.get("password"));
+		oc.click_Login();
+		oc.Clickon_Admin();
+		oc.Clickon_Configuration();
+		Thread.sleep(3000);
+		oc.Clickon_Modules();
+		Thread.sleep(3000);
+		oc.Clickon_LeaveModule();
+		Thread.sleep(3000);
+		oc.Clickon_TimeModule();
+		Thread.sleep(3000);
+		oc.Clickon_RecruitmentModule();
+		Thread.sleep(3000);
+		oc.Clickon_PerformanceModule();
+		Thread.sleep(3000);
+		oc.Clickon_DirectoryModule();
+		Thread.sleep(3000);
+		oc.Clickon_maintananceModule();
+		Thread.sleep(3000);
+		oc.Clickon_MobileModule();
+		Thread.sleep(3000);
+		oc.Clickon_ClaimModule();
+		Thread.sleep(3000);
+		oc.Clickon_ModuleSave();
+		Thread.sleep(5000);
+	}
+	
+	
+	@Test(priority=8 , dataProvider="setData0")
+	public void ClickonSocialMediaAuthentication(Hashtable<String, String>h1) throws Throwable
+	{
+		extentLoggerECP = parentExtentLogger.createNode("user can click on SocialMediaAuthentication ");
+		
+		oc = new OrangeHRM_LoginPage(GenericKeywordsWithPage.driver);
+		
+		oc.enter_Username(h1.get("userName"));
+		oc.enter_Password(h1.get("password"));
+		oc.click_Login();
+		oc.Clickon_Admin();
+		oc.Clickon_Configuration();
+		Thread.sleep(3000);
+		oc.SocialMediaAuthentication();
+		oc.SocialAddBtn();
+		oc.NameInputBox(h1.get("SocialName"));
+		oc.ProviderInputBox(h1.get("ProvideURL"));
+		oc.ClientInputBox(h1.get("ClientID"));
+		oc.ClientsecretInputBox(h1.get("ClientSecret"));
+		oc.Clickon_SocialSave();
+		Thread.sleep(15000);
+	}
+	
+	@Test(priority=9 , dataProvider="setData0")
+	public void ClickAgainOnModules(Hashtable<String, String>h1) throws Throwable
+	{
+		extentLoggerECP = parentExtentLogger.createNode("user can click Again on Modules ");
+		
+		oc = new OrangeHRM_LoginPage(GenericKeywordsWithPage.driver);
+		
+		oc.enter_Username(h1.get("userName"));
+		oc.enter_Password(h1.get("password"));
+		oc.click_Login();
+		oc.Clickon_Admin();
+		oc.Clickon_Configuration();
+		Thread.sleep(3000);
+		oc.SocialMediaAuthentication();
+		oc.Clickon_EditBtn();
+		Thread.sleep(3000);
+		oc.EditNameInputBox(h1.get("UpdateSocialName"));
+		oc.EditProviderInputBox(h1.get("UpdateProvideURL"));
+		oc.EditClientInputBox(h1.get("UpdateClientID"));
+		oc.EditClientsecretInputBox(h1.get("UpdateClientSecret"));
+		oc.Clickon_SocialSave();
+		Thread.sleep(15000);
+	}
+	
+	@Test(priority=10 , dataProvider="setData0")
+	public void ClickonRegisterOAuthClient(Hashtable<String, String>h1) throws Throwable
+	{
+		extentLoggerECP = parentExtentLogger.createNode("user can click Again on RegisterOAuthClient ");
+		
+		oc = new OrangeHRM_LoginPage(GenericKeywordsWithPage.driver);
+		
+		oc.enter_Username(h1.get("userName"));
+		oc.enter_Password(h1.get("password"));
+		oc.click_Login();
+		oc.Clickon_Admin();
+		oc.Clickon_Configuration();
+		Thread.sleep(3000);
+		oc.RegisterOAuthClient();
+		oc.OAuthAddbtn();
+		oc.Enter_OAuthInputName(h1.get("OAuthName"));
+		oc.Enter_RedirectURIName(h1.get("RequiredURI"));
+		oc.OAuthSavebtn();
+		Thread.sleep(15000);
+	}
 }
